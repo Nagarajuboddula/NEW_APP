becb852fd1bb7a5385b29a73139c30045d7ff457
Nagarajuboddula <naga.boddula@outlook.com>
2023-09-09 23:16:40 +0530

    assacfvs

---
+++ main.tf
@@ -0,0 +1,70 @@
+/*terraform {
+required_version = “>=0.12”
+}
+provider “aws” {
+#profile = var.profile
+shared_credentials_files = ["/root/.aws/credentials"]
+region = us-east-1
+}*/
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+resource "aws_instance" "ec2_example" {
+
+    ami = "ami-053b0d53c279acc90"
+    instance_type = "t2.micro"
+    key_name= "naga"
+    #vpc_security_group_ids = [aws_security_group.main.id]
+/*
+  user_data = <<-EOF
+      #!/bin/sh
+      sudo apt-get update
+      sudo apt install -y apache2
+      sudo systemctl status apache2
+      sudo systemctl start apache2
+      sudo chown -R $USER:$USER /var/www/html
+      sudo echo "<html><body><h1>Hello this custom page built with Terraform User Data</h1></body></html>" > /var/www/html/index.html
+      EOF*/ 
+} 
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+}
+
